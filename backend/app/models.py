3cc9489763c5d3f05563392e616baf402c72acdc
ZakPorat0 <zakporat2023@gmail.com>
2024-06-29 23:36:08 +0100

    feat: add verification code model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,10 +1,23 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
 
 class UserModel(Base):
-    __tablename__ = 'user'
+    __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String, unique=True)
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
     password = Column(String)
-    is_admin = Column(Boolean, default=False)
+    is_verified = Column(Boolean, default=False)
+
+    verification_codes = relationship("VerificationCodeModel", back_populates="user")
+
+class VerificationCodeModel(Base):
+    __tablename__ = "verification_codes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    code = Column(String)
+    expires_at = Column(DateTime)
+
+    user = relationship("UserModel", back_populates="verification_codes")
