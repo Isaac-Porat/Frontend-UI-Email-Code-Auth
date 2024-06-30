2961087e03788a1dced603691ed6b807582cd859
ZakPorat0 <zakporat2023@gmail.com>
2024-06-30 13:08:40 +0100

    refractor: convert app router to API router

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,71 +1,25 @@
-import os
 import logging
 from datetime import datetime, timedelta
-from fastapi import Depends, HTTPException, status, Request
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status, APIRouter
 from passlib.context import CryptContext
-import jwt
 from sqlalchemy.orm import Session
-from schemas import Token, UserSchema
 from models import UserModel, VerificationCodeModel
 from database import engine
 from dotenv import load_dotenv
-from schemas import UserUpdate
-
+from schemas import UserUpdate, UserSchema, VerifyCodeResponse, Token
+from auth_utils import get_password_hash, create_access_token, send_verification_email, get_current_user
 from datetime import datetime, timedelta
 import secrets
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 
 load_dotenv()
 
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
 logger = logging.getLogger("uvicorn")
 
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-HASHING_ALGORITHM = os.getenv("HASHING_ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+router = APIRouter()
 
-EMAIL_USER = os.getenv("EMAIL_USER")
-EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
-
-pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-def create_access_token(data: dict):
-    to_encode = data.copy()
-
-    expire = datetime.utcnow() + timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
-    to_encode.update({"exp": expire})
-
-    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=HASHING_ALGORITHM)
-
-    return encoded_jwt
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    with Session(engine) as session:
-        credentials_exception = HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-        try:
-            payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[HASHING_ALGORITHM])
-            email: str = payload.get("sub")
-            if email is None:
-                raise credentials_exception
-            user = session.query(UserModel).filter(UserModel.email == email).first()
-            if user is None:
-                raise credentials_exception
-        except jwt.PyJWTError:
-            raise credentials_exception
-
-        return email
-
+@router.post('/register')
 async def register_user(user: UserSchema):
 
     with Session(engine) as session:
@@ -108,6 +62,38 @@ async def register_user(user: UserSchema):
                 detail="An unexpected error occurred"
             )
 
+@router.post('/verify-verification-code', response_model=Token)
+async def verify_verification_code(data: VerifyCodeResponse) -> Token:
+    try:
+        with Session(engine) as session:
+            user = session.query(UserModel).filter(UserModel.email == data.email).first()
+            if not user:
+                raise HTTPException(status_code=404, detail="User not found")
+
+            verification = session.query(VerificationCodeModel).filter(
+                VerificationCodeModel.user_id == user.id,
+                VerificationCodeModel.code == data.code,
+                VerificationCodeModel.expires_at > datetime.utcnow()
+            ).first()
+
+            if not verification:
+                raise HTTPException(status_code=400, detail="Invalid or expired verification code")
+
+            user.is_verified = True
+            session.delete(verification)
+            session.commit()
+
+            access_token = create_access_token(data={"sub": user.email})
+            return Token(access_token=access_token, token_type="bearer")
+
+    except HTTPException as e:
+        raise e
+
+    except Exception as e:
+        logger.error(f"Unexpected error during user update: {e}", exc_info=True)
+        raise HTTPException(status_code=500, detail="An unexpected error occurred")
+
+@router.post('/login', response_model=Token)
 async def login_user(user: UserSchema = Depends()) -> Token:
     with Session(engine) as session:
         try:
@@ -141,7 +127,46 @@ async def login_user(user: UserSchema = Depends()) -> Token:
                 detail="An unexpected error occurred"
             )
 
-async def get_user_data(token: str) -> UserSchema:
+@router.post('/token', response_model=Token) # route for FastAPI docs
+async def token(user: UserSchema = Depends()) -> Token:
+    with Session(engine) as session:
+        try:
+            queryUser = session.query(UserModel).filter(UserModel.email == user.email).first()
+
+            if queryUser is None:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Incorrect username or password",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+
+            if not pwd_context.verify(user.password, queryUser.password):
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Incorrect username or password",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+
+            access_token = create_access_token(data={"sub": str(user.email)})
+
+            return Token(access_token=access_token, token_type="bearer")
+
+        except HTTPException as e:
+            raise e
+
+        except Exception as e:
+            logger.error(f"Unexpected error during login: {e}", exc_info=True)
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="An unexpected error occurred"
+            )
+
+@router.get('/users/me')
+async def get_current_active_user(token: str = Depends(get_current_user)):
+    return token # returns user's email
+
+@router.get('/get-user-data', response_model=UserSchema)
+async def get_user_data(token: str = Depends(get_current_user)) -> UserSchema:
     with Session(engine) as session:
         try:
             user = session.query(UserModel).filter(UserModel.email == token).first()
@@ -167,10 +192,11 @@ async def get_user_data(token: str) -> UserSchema:
                 detail="An unexpected error occurred while fetching user data"
             )
 
-async def update_user_data(email: str, user_update: UserUpdate) -> UserSchema:
+@router.put('/update-user', response_model=UserSchema)
+async def update_user_data(user_update: UserUpdate, token: str = Depends(get_current_user)) -> UserSchema:
     try:
         with Session(engine) as session:
-            user = session.query(UserModel).filter(UserModel.email == email).first()
+            user = session.query(UserModel).filter(UserModel.email == token).first()
             if not user:
                 raise HTTPException(
                     status_code=404,
@@ -198,72 +224,6 @@ async def update_user_data(email: str, user_update: UserUpdate) -> UserSchema:
             )
 
             return {"message": "User updated successfully", "user": updated_user}
-
-    except HTTPException as e:
-        raise e
-
-    except Exception as e:
-        logger.error(f"Unexpected error during user update: {e}", exc_info=True)
-        raise HTTPException(status_code=500, detail="An unexpected error occurred")
-
-def send_verification_email(email: str, code: str):
-    smtp_server = "smtp.gmail.com"
-    port = 587  # For starttls
-    sender_email = EMAIL_USER
-    password = EMAIL_PASSWORD
-
-    message = MIMEMultipart("alternative")
-    message["Subject"] = "Your Verification Code"
-    message["From"] = sender_email
-    message["To"] = email
-
-    text = f"Your verification code is: {code}"
-    html = f"""\
-    <html>
-      <body>
-        <p>Your verification code is: <strong>{code}</strong></p>
-      </body>
-    </html>
-    """
-
-    part1 = MIMEText(text, "plain")
-    part2 = MIMEText(html, "html")
-
-    message.attach(part1)
-    message.attach(part2)
-
-    try:
-        with smtplib.SMTP(smtp_server, port) as server:
-            server.starttls()
-            server.login(sender_email, password)
-            server.sendmail(sender_email, email, message.as_string())
-        logger.warning(f"Verification email sent to {email}")
-    except Exception as e:
-        logger.warning(f"Error sending email: {str(e)}")
-        raise
-
-async def verify(email: str, code: str):
-    try:
-        with Session(engine) as session:
-            user = session.query(UserModel).filter(UserModel.email == email).first()
-            if not user:
-                raise HTTPException(status_code=404, detail="User not found")
-
-            verification = session.query(VerificationCodeModel).filter(
-                VerificationCodeModel.user_id == user.id,
-                VerificationCodeModel.code == code,
-                VerificationCodeModel.expires_at > datetime.utcnow()
-            ).first()
-
-            if not verification:
-                raise HTTPException(status_code=400, detail="Invalid or expired verification code")
-
-            user.is_verified = True
-            session.delete(verification)
-            session.commit()
-
-            access_token = create_access_token(data={"sub": user.email})
-            return {"access_token": access_token, "token_type": "bearer"}
 
     except HTTPException as e:
         raise e
