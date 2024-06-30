5aeba846b4f28ef476534d74ccb109e39b0c0dc6
ZakPorat0 <zakporat2023@gmail.com>
2024-06-30 13:25:55 +0100

    refractor: add is_admin to UserModel

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,6 +9,7 @@ class UserModel(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String)
     is_verified = Column(Boolean, default=False)
+    is_admin = Column(Boolean, default=False)
 
     verification_codes = relationship("VerificationCodeModel", back_populates="user")
 
