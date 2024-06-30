8aac2e426a1273f2d27de1e0cd89c22965c3e971
ZakPorat0 <zakporat2023@gmail.com>
2024-06-30 13:09:37 +0100

    style: separate util functions from auth file

---
+++ backend/app/auth_utils.py
@@ -0,0 +1,100 @@
+import os
+import logging
+from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+import jwt
+from sqlalchemy.orm import Session
+from models import UserModel
+from database import engine
+from dotenv import load_dotenv
+
+from datetime import datetime, timedelta
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+load_dotenv()
+
+logger = logging.getLogger("uvicorn")
+
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+HASHING_ALGORITHM = os.getenv("HASHING_ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=HASHING_ALGORITHM)
+
+    return encoded_jwt
+
+def send_verification_email(email: str, code: str):
+    smtp_server = "smtp.gmail.com"
+    port = 587  # For starttls
+    sender_email = EMAIL_USER
+    password = EMAIL_PASSWORD
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Your Verification Code"
+    message["From"] = sender_email
+    message["To"] = email
+
+    text = f"Your verification code is: {code}"
+    html = f"""\
+    <html>
+      <body>
+        <p>Your verification code is: <strong>{code}</strong></p>
+      </body>
+    </html>
+    """
+
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    message.attach(part1)
+    message.attach(part2)
+
+    try:
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.starttls()
+            server.login(sender_email, password)
+            server.sendmail(sender_email, email, message.as_string())
+        logger.warning(f"Verification email sent to {email}")
+    except Exception as e:
+        logger.warning(f"Error sending email: {str(e)}")
+        raise
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    with Session(engine) as session:
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+        try:
+            payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[HASHING_ALGORITHM])
+            email: str = payload.get("sub")
+            if email is None:
+                raise credentials_exception
+            user = session.query(UserModel).filter(UserModel.email == email).first()
+            if user is None:
+                raise credentials_exception
+        except jwt.PyJWTError:
+            raise credentials_exception
+
+        return email
